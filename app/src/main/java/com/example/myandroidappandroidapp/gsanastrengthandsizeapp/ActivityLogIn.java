6c3b79f6419180b7719981e02328b20261f709e9
Gareth Sanashee <gareth.sanashee@appsinthesky.com>
2019-08-11 19:04:26 +0100

    create log in activity

--- app/src/main/java/com/example/myandroidappandroidapp/gsanastrengthandsizeapp/ActivityLogIn.java
+++ app/src/main/java/com/example/myandroidappandroidapp/gsanastrengthandsizeapp/ActivityLogIn.java
@@ -1,18 +1,13 @@
 package com.example.myandroidappandroidapp.gsanastrengthandsizeapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivityLogIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, ActivityLogIn.class);
-        startActivity(intent);
-
+        setContentView(R.layout.activity_log_in);
     }
 }
