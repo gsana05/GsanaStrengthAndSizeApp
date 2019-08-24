a3929d71ddff7a25265e8415ac0c26eaa82df7a4
Gareth Sanashee <gareth.sanashee@appsinthesky.com>
2019-08-24 12:15:34 +0100

    merged log-in-ui branch into master

--- app/src/main/java/com/example/myandroidappandroidapp/gsanastrengthandsizeapp/MainActivity.java
+++ app/src/main/java/com/example/myandroidappandroidapp/gsanastrengthandsizeapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
