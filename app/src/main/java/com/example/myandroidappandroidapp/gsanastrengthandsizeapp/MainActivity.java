2f65ea45fadd098d8ba14cd6cd6a59146f9b1575
Gareth Sanashee <gareth.sanashee@appsinthesky.com>
2019-08-26 19:21:56 +0100

    importing libs

--- app/src/main/java/com/example/myandroidappandroidapp/gsanastrengthandsizeapp/MainActivity.java
+++ app/src/main/java/com/example/myandroidappandroidapp/gsanastrengthandsizeapp/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.myandroidappandroidapp.gsanastrengthandsizeapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
