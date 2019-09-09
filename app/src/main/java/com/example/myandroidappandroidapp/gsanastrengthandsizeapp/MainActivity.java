77d39c4fd0ddabc8c703be28c26967b2144a3422
Gareth Sanashee <gareth.sanashee@appsinthesky.com>
2019-09-09 20:10:24 +0100

    put log out function in singleton

--- app/src/main/java/com/example/myandroidappandroidapp/gsanastrengthandsizeapp/MainActivity.java
+++ app/src/main/java/com/example/myandroidappandroidapp/gsanastrengthandsizeapp/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.example.myandroidappandroidapp.gsanastrengthandsizeapp.models.DataModelResult;
+import com.example.myandroidappandroidapp.gsanastrengthandsizeapp.models.UserModelSingleton;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,8 +25,24 @@ public class MainActivity extends AppCompatActivity {
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity();
+
+                DataModelResult<Boolean> callback = new DataModelResult<Boolean>() {
+                    @Override
+                    public void onComplete(Boolean data, Exception exception) {
+                        if(data){
+                            startActivity();
+                        }
+                        else {
+                            Log.v("LOG OUT ERROR", "Something went wrong logging out");
+                        }
+
+                    }
+                };
+
+                UserModelSingleton userModelSingleton = UserModelSingleton.getInstance();
+                userModelSingleton.logout(callback);
+
+
             }
         });
     }
