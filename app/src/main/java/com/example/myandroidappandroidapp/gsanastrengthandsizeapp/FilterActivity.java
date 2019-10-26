44e781fbdbd52ffd7b81d5392e2d7f9619c9ec12
Gareth Sanashee <gareth.sanashee@appsinthesky.com>
2019-10-26 13:26:24 +0100

    custom switch for filtering league table

--- app/src/main/java/com/example/myandroidappandroidapp/gsanastrengthandsizeapp/FilterActivity.java
+++ app/src/main/java/com/example/myandroidappandroidapp/gsanastrengthandsizeapp/FilterActivity.java
@@ -4,15 +4,21 @@ import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.TextView;
@@ -27,6 +33,16 @@ public class FilterActivity extends AppCompatActivity {
 
     private Spinner spinner;
     private SpinnerAdapter mAdapter;
+    private Spinner customerSpinnerbench;
+    private SpinnerAdapter customSpinnerAdapterBench;
+    private ImageView spinnerDropDownBenchIcon;
+
+    private Button customiseBtn;
+    private Boolean isCustom = false;
+    private ImageView spinnerDropDownIcon;
+    private SwitchCompat switchCompat;
+    private TextView tvStandard;
+    private TextView tvCustom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +56,7 @@ public class FilterActivity extends AppCompatActivity {
         list.add(new Filter(3, "Deadlift"));
         list.add(new Filter(4, "Over Head Press"));
 
+        spinnerDropDownIcon = this.findViewById(R.id.filter_main_view_drop_down_arrow);
         spinner = this.findViewById(R.id.filter_main_view_drop_down);
         mAdapter = new CustomDropDownAdapter(this, list);
         spinner.setAdapter(mAdapter);
@@ -74,6 +91,32 @@ public class FilterActivity extends AppCompatActivity {
 
             }
         });
+
+        ArrayList<Filter> benchList = new ArrayList<>();
+        benchList.add(null);
+        benchList.add(new Filter(1, "Bench Press"));
+
+        spinnerDropDownBenchIcon = this.findViewById(R.id.filter_main_view_drop_down_custom_bench_arrow);
+        customerSpinnerbench = this.findViewById(R.id.filter_main_view_drop_down_custom_bench);
+        customSpinnerAdapterBench = new CustomDropDownAdapter(this, benchList);
+        customerSpinnerbench.setAdapter(customSpinnerAdapterBench);
+        customerSpinnerbench.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Filter i = (Filter) parent.getItemAtPosition(position);
+                if(i != null){
+                    Toast.makeText(parent.getContext(), i.getName(), Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(parent.getContext(), "All", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
 
        /* spinner = this.findViewById(R.id.filter_main_view_drop_down);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sort_string_array, android.R.layout.simple_spinner_item);
@@ -113,6 +156,49 @@ public class FilterActivity extends AppCompatActivity {
                 finish();
             }
         });*/
+
+       /* customiseBtn = this.findViewById(R.id.filter_man_view_custom_btn);
+        customiseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isCustom = true;
+                updateUI();
+            }
+        });*/
+
+        switchCompat = this.findViewById(R.id.filter_main_view_switch);
+        switchCompat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUI();
+            }
+        });
+
+        tvStandard = this.findViewById(R.id.dashboard_map_toggle_switch_map);
+        tvCustom = this.findViewById(R.id.dashboard_map_toggle_switch_list);
+
+        updateUI();
+    }
+
+    public void updateUI(){
+
+        if(switchCompat.isChecked()){
+            tvStandard.setVisibility(View.INVISIBLE);
+            tvCustom.setVisibility(View.VISIBLE);
+            spinner.setVisibility(View.GONE);
+            spinnerDropDownIcon.setVisibility(View.GONE);
+            customerSpinnerbench.setVisibility(View.VISIBLE);
+            spinnerDropDownBenchIcon.setVisibility(View.VISIBLE);
+        }
+        else {
+            tvStandard.setVisibility(View.VISIBLE);
+            tvCustom.setVisibility(View.INVISIBLE);
+            spinner.setVisibility(View.VISIBLE);
+            spinnerDropDownIcon.setVisibility(View.VISIBLE);
+
+            customerSpinnerbench.setVisibility(View.GONE);
+            spinnerDropDownBenchIcon.setVisibility(View.GONE);
+        }
     }
 }
 
